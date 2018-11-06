39b5899925392c3b9d0c7b9f621fee675c2bede8
MCClarke <mcarlotclarke@gmail.com>
2018-11-06 14:57:05 -0500

    Added current directory

--- bash.js
+++ bash.js
@@ -5,6 +5,12 @@ process.stdout.write("prompt > ");
 process.stdin.on("data", data => {
   const cmd = data.toString().trim(); // remove the newline
 
-  process.stdout.write("You typed: " + cmd);
+  if (cmd === 'pwd'){
+    // process.stdout.write(__dirname);
+    process.stdout.write(`Current directory: ${process.cwd()}`);
+  } else {
+    process.stdout.write("You typed: " + cmd);
+  }
+
   process.stdout.write("\nprompt > ");
 });
